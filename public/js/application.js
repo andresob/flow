3e6ba08080d4c448d2e48b72f4dd45d4048083be
Andre <andresob1213@gmail.com>
2014-03-25 15:33:48 -0300

    activated link works

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,3 @@
 $(document).ready(function(){
-  var url = location.pathname.split("/")[1];
-  $('.side-nav li a[href^="/' + url + '"]').addClass('active');
+  $('.side-nav li a[href$="' + location.pathname.split("/")[1] + '"]').addClass('active');
 });
