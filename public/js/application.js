573e502f6ea15199e0489bc993834f1df54a1bce
Andre <andresob1213@gmail.com>
2014-03-26 11:45:58 -0300

    provisorio

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,25 @@
+function search_node(attr, value) {
+  return nodes.filter(function(n) {
+    return n.name.toLowerCase().indexOf(value.toLowerCase()) >= 0;
+  });
+};
+
+function get_node_name(attr, value) {
+  return search_node(attr, value).map(function(n) {
+    return n.name;
+  });
+};
+
 $(document).ready(function(){
+  $("#myTags").tagit({
+    tagSource: function(request, response){
+      response(get_node_name("name", request.term));
+    },
+    tagLimit: 1,
+    allowSpaces: true,
+    onlyAvailableTags: true,
+    removeConfirmation: true,
+    autocomplete: {minLength: 3},
+  });
   $('.side-nav li a[href$="' + location.pathname.split("/")[1] + '"]').addClass('active');
 });
