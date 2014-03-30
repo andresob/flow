02a558fcc1f5b65ecf32d19e884df8d4db7402ae
Andre <andresob1213@gmail.com>
2014-03-30 16:10:20 -0300

    deformed map a caminho

--- public/js/deformed.js
+++ public/js/deformed.js
@@ -13,14 +13,48 @@ var svg = d3.select("#deformed").insert("svg:svg")
     .attr("width", width - 50)
     .attr("height", height -100);
 
-var state = svg.append("svg:g")
-    .attr("id", "state");
+var state = svg.append("g")
+    .attr("id", "state")
+    .selectAll("path");
 
-//carrega o arquivo para desenhar o poligono
-d3.json("data/brasil.topo.json", function(error, collection) {
-  state.selectAll("path")
-      .data(topojson.feature(collection, collection.objects.states).features)
-    .enter().append("svg:path")
-      .attr("class", function(d) { return "state " + d.properties.regiao_id;})
-      .attr("d", path);
+var topology,
+    geometries,
+    carto_features;
+
+var carto = d3.cartogram()
+    .projection(projection)
+    .properties(function (d) { return d.properties; });
+
+d3.json("data/brasil.topo.json", function (data) {
+    topology = data;
+    geometries = topology.objects.states.geometries;
+
+    var features = carto.features(topology, geometries);
+
+    state = state.data(features)
+        .enter()
+        .append("path")
+        .attr("class", "munic")
+        .attr("id", function (d) {
+            return d.properties.nome;
+        })
+        .attr("d", path);
+
+    d3.select("#click_to_run").text("click here to run");
+        console.log(carto_features);
 });
+
+function do_update() {
+    d3.select("#click_to_run").text("thinking...");
+    setTimeout(function () {
+
+        if (carto_features == undefined) {
+            carto_features = carto(topology, geometries).features;
+        }
+
+        state.data(carto_features)
+            .transition()
+            .duration(750)
+            .attr("d", carto.path);
+    }, 10);
+}
