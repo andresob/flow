ce2795f2fced16f952fcdf5356d4af16f233e495
Andre <andresob1213@gmail.com>
2014-03-31 21:51:26 -0300

    deformed working partialy

--- public/js/deformed.js
+++ public/js/deformed.js
@@ -13,6 +13,8 @@ var svg = d3.select("#deformed").insert("svg:svg")
     .attr("width", width - 50)
     .attr("height", height -100);
 
+var map = d3.select("#deformed")
+
 var state = svg.append("g")
     .attr("id", "state")
     .selectAll("path");
@@ -24,6 +26,8 @@ var topology,
 var carto = d3.cartogram()
     .projection(projection)
     .properties(function (d) { return d.properties; });
+
+var vote_data = d3.map();
 
 d3.json("data/brasil.topo.json", function (data) {
     topology = data;
@@ -40,14 +44,24 @@ d3.json("data/brasil.topo.json", function (data) {
         })
         .attr("d", path);
 
-    d3.select("#click_to_run").text("click here to run");
-        console.log(carto_features);
+    d3.select("#click_to_run").text("Clique AQUI para iniciar");
+});
+
+d3.csv("data/brasil_def.csv", function (data) {
+  data.forEach(function (d) {
+      vote_data.set(d.STATE,[d.INDEX, d.NAME]);
+      console.log(vote_data);
+  })
 });
 
 function do_update() {
-    d3.select("#click_to_run").text("thinking...");
+    d3.select("#click_to_run").text("aguarde...");
     setTimeout(function () {
-
+      
+        carto.value(function (d) {
+            return +vote_data.get(d.properties["id"])[0];
+        });
+                
         if (carto_features == undefined) {
             carto_features = carto(topology, geometries).features;
         }
